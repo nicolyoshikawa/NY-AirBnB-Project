e6ca6e87f94cc06626e9b6ca16f12ad2850799b2
Nicol Yoshikawa <nicolyoshikawa@Nicols-MBP.attlocal.net>
2023-04-08 14:22:23 -0700

    Add an API router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,11 +2,16 @@
 const express = require('express');
 const router = express.Router();
 
+// backend/routes/index.js
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
+
 router.get("/api/csrf/restore", (req, res) => {
   const csrfToken = req.csrfToken();
   res.cookie("XSRF-TOKEN", csrfToken);
   res.status(200).json({
-    'XSRF-Token': csrfToken
+    'XSRF-TOKEN': csrfToken
   });
 });
 
