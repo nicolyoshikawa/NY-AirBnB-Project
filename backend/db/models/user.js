d479e2d91b3554383b1ee76de5fe9490ac490b82
Nicol Yoshikawa <nicolyoshikawa@Nicols-MBP.attlocal.net>
2023-04-12 21:18:32 -0700

    added user model security

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -52,6 +52,11 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'User',
+    defaultScope: {
+      attributes: {
+        exclude: ["password", "email", "createdAt", "updatedAt"]
+      }
+    }
   });
   return User;
 };
