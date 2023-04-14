caf41140c36b9d1abc64af10507f96e2773a0d0e
Nicol Yoshikawa <nicolyoshikawa@Nicols-MBP.attlocal.net>
2023-04-13 20:48:05 -0700

    Add User logout backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -10,37 +10,43 @@ const router = express.Router();
 
 // Log in
 router.post('/', async (req, res, next) => {
-      const { credential, password } = req.body;
+  const { credential, password } = req.body;
 
-      const user = await User.unscoped().findOne({
-        where: {
-          [Op.or]: {
-            username: credential,
-            email: credential
-          }
-        }
-      });
-
-      if (!user || !bcrypt.compareSync(password, user.password.toString())) {
-        const err = new Error('Login failed');
-        err.status = 401;
-        err.title = 'Login failed';
-        err.errors = { credential: 'The provided credentials were invalid.' };
-        return next(err);
+  const user = await User.unscoped().findOne({
+    where: {
+      [Op.or]: {
+        username: credential,
+        email: credential
       }
-
-      const safeUser = {
-        id: user.id,
-        email: user.email,
-        username: user.username,
-      };
-
-      await setTokenCookie(res, safeUser);
-
-      return res.json({
-        user: safeUser
-      });
     }
-  );
+  });
+
+  if (!user || !bcrypt.compareSync(password, user.password.toString())) {
+    const err = new Error('Login failed');
+    err.status = 401;
+    err.title = 'Login failed';
+    err.errors = { credential: 'The provided credentials were invalid.' };
+    return next(err);
+  }
+
+  const safeUser = {
+    id: user.id,
+    email: user.email,
+    username: user.username,
+  };
+
+  await setTokenCookie(res, safeUser);
+
+  return res.json({
+    user: safeUser
+  });
+});
+
+// Log out
+router.delete('/', (_req, res) => {
+    res.clearCookie('token');
+    return res.json({ message: 'success' });
+  }
+);
 
 module.exports = router;
