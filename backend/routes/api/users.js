92f0d93483f875f4b7be8a7a239619f9c83b5eb5
Nicol Yoshikawa <nicolyoshikawa@Nicols-MBP.attlocal.net>
2023-04-17 21:59:18 -0700

    added token to sign up user response

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -32,18 +32,79 @@ const validateSignup = [
 // Sign up
 router.post('', validateSignup, async (req, res) => {
     const { email, password, username, firstName, lastName } = req.body;
+
+    //User already exists with the specified email
+    // let emailExists = await User.findOne({ where: {email}});
+    // if(emailExists) {
+    //   res.status(403);
+    //   return res.json({
+    //     "message": "User already exists",
+    //     "statusCode": 403,
+    //     "errors": [
+    //       "User with that email already exists"
+    //     ]
+    //   });
+    // };
+
+    //User already exists with the specified username
+    // let usernameExists = await User.findOne({ where: {username}});
+    // if(usernameExists) {
+    //   res.status(403);
+    //   return res.json({
+    //     "message": "User already exists",
+    //     "statusCode": 403,
+    //     "errors": [
+    //       "User with that username already exists"
+    //     ]
+    //   });
+    // }
+
+    //Body validation errors
+    // if(!username) {
+    //   res.status(400);
+    //   return res.json({
+    //     "message": "Validation error",
+    //     "statusCode": 400,
+    //     "errors": [
+    //       "Username is required"
+    //     ]
+    //   });
+    // }
+    // if(!firstName) {
+    //   res.status(400);
+    //   return res.json({
+    //     "message": "Validation error",
+    //     "statusCode": 400,
+    //     "errors": [
+    //       "First Name is required"
+    //     ]
+    //   });
+    // }
+    // if (!lastName) {
+    //   res.status(400);
+    //   return res.json({
+    //     "message": "Validation error",
+    //     "statusCode": 400,
+    //     "errors": [
+    //       "Last Name is required"
+    //     ]
+    //   });
+    // }
+
     const hashedPassword = bcrypt.hashSync(password);
     const user = await User.create({ email, username, password: hashedPassword, firstName, lastName });
 
     const safeUser = {
-    id: user.id,
-    email: user.email,
-    firstName: user.firstName,
-    lastName: user.lastName,
-    username: user.username
+      id: user.id,
+      email: user.email,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      username: user.username
     };
 
-    await setTokenCookie(res, safeUser);
+    let token = await setTokenCookie(res, safeUser);
+
+    safeUser.token = token;
 
     return res.json({
         user: safeUser
