57b9a53074bd06248c93fc7e1ffd1610811f9d5a
Nicol Yoshikawa <nicolyoshikawa@Nicols-MBP.attlocal.net>
2023-04-22 18:01:30 -0700

    edited migration name of bookings to run after user and spot

--- backend/db/seeders/20230422180328-reviewImages.js
+++ backend/db/seeders/20230422180328-reviewImages.js
@@ -14,15 +14,15 @@ module.exports = {
     return queryInterface.bulkInsert(options, [
       {
         url: "https://user-images.githubusercontent.com/112415366/233506013-ed9f81aa-82ba-4285-a41c-8dd3b5e720fe.png",
-        reviewId: 6
+        reviewId: 1
       },
       {
         url: "https://user-images.githubusercontent.com/112415366/233506138-6779fbf9-3557-402a-9510-757cc4ec10dc.jpeg",
-        reviewId: 6
+        reviewId: 1
       },
       {
         url: "https://user-images.githubusercontent.com/112415366/233506288-b08a9fa2-7148-402c-b30a-83ae65fab4a6.jpeg",
-        reviewId: 6
+        reviewId: 1
       },
     ], {});
   },
