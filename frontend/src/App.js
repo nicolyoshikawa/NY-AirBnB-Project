ef04326036838ac6a65e1d7ac4aac9cc603d81e1
Nicol Yoshikawa <nicolyoshikawa@Nicols-MBP.attlocal.net>
2023-05-26 01:08:59 -0700

    login and sign up complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,28 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Route, Switch } from "react-router-dom";
+import LoginFormPage from "./components/LoginFormPage";
+import SignupFormPage from "./components/SignupFormPage";
+import * as sessionActions from "./store/session";
+
 function App() {
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+  useEffect(() => {
+    dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
+  }, [dispatch]);
+
   return (
-    <h1>Hello from App</h1>
+    isLoaded && (
+      <Switch>
+        <Route path="/session">
+          <LoginFormPage />
+        </Route>
+        <Route path="/users">
+          <SignupFormPage />
+        </Route>
+      </Switch>
+    )
   );
 }
 
