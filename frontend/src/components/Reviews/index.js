ef64a5888ce76c600f4bde9c78889ef1250192ce
Nicol Yoshikawa <nicolyoshikawa@Nicols-MBP.attlocal.net>
2023-05-31 23:02:41 -0700

    completed feature view spot detail functionality

--- frontend/src/components/Reviews/index.js
+++ frontend/src/components/Reviews/index.js
@@ -2,18 +2,22 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import * as reviewActions from "../../store/reviews.js";
 import ReviewList from "./ReviewList.js";
+import ReviewAvg from "./ReviewAvg.js";
 import "./Review.css";
 
-const SpotReviews = ({spotId}) => {
+const Reviews = ({spot}) => {
     const dispatch = useDispatch();
     const allReviews = useSelector(state => Object.values(state.reviews));
-
+    console.log(spot)
     useEffect(()=> {
-        dispatch(reviewActions.loadAllReviews(spotId));
-    },[dispatch, spotId]);
+        dispatch(reviewActions.loadAllReviews(spot.id));
+    },[dispatch, spot.id]);
 
     return(
         <>
+            <div>
+                <ReviewAvg spot={spot}/>
+            </div>
             <div>
                 {allReviews.map(el => (<ReviewList key={el.id} reviewObj={el}/>))}
             </div>
@@ -21,4 +25,4 @@ const SpotReviews = ({spotId}) => {
     )
 };
 
-export default SpotReviews;
+export default Reviews;
--- frontend/src/components/Spots/SpotDetails.js
+++ frontend/src/components/Spots/SpotDetails.js
@@ -1,33 +1,36 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
 import { useParams } from 'react-router-dom';
-import SpotReviews from "../Reviews/index.js";
+import Reviews from "../Reviews/index.js";
 import SpotImages from "./SpotImages.js";
+import CallOutBox from "./CallOutBox.js";
 import * as spotActions from "../../store/spots";
 import "./Spots.css";
 
 const SpotDetails = () => {
     const { spotId } = useParams();
     const spotsObj = useSelector(state => state.spots);
+    const [isLoaded, setIsLoaded] = useState(false);
     const spot = spotsObj[spotId];
     const dispatch = useDispatch();
 
     useEffect(()=> {
-        dispatch(spotActions.loadSpotById(spotId));
+        dispatch(spotActions.loadSpotById(spotId))
+        .then(()=>setIsLoaded(true))
     },[dispatch, spotId]);
     return (
         <>
-            {spot ? (
+            {isLoaded && (
                 <>
                     <h1 className="allSpots">{spot.name}</h1>
                     <div className="allSpots">{spot.city}, {spot.state}, {spot.country}</div>
                     <div className="allSpots"><SpotImages spot={spot}/></div>
+                    <div className="callOutBox"><CallOutBox spot={spot}/></div>
                     <div className="allSpots">Hosted by {spot.Owner.firstName} {spot.Owner.lastName}</div>
-                    <div className="allSpots">{spot.description}</div>
-                    <div><SpotReviews spotId={spotId} /></div>
+                    <p className="allSpots">{spot.description}</p>
+                    <div><Reviews spot={spot}/></div>
                 </>
-            ) : (
-                <></>
             )}
         </>
     )
