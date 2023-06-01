ab3729240938aa38e46c9f92a6ca85666ec82be0
Nicol Yoshikawa <nicolyoshikawa@Nicols-MBP.attlocal.net>
2023-06-01 01:38:23 -0700

    view rating review feature functionality complete

--- frontend/src/components/Reviews/ReviewAvg.js
+++ frontend/src/components/Reviews/ReviewAvg.js
@@ -2,12 +2,30 @@ import "./Review.css";
 
 const ReviewAvg = ({spot}) => {
     const avgRating = spot.avgStarRating ? spot.avgStarRating.toFixed(2) : "New";
+    let reviewLine;
+    if(spot.numReviews === 1){
+        reviewLine = (
+            <>
+                <i className="fa-solid fa-circle fa-2xs"></i>
+                <div className="reviewAvg">{spot.numReviews} review</div>
+            </>
+        )
+    } else if (spot.numReviews > 1) {
+        reviewLine = (
+            <>
+                <i className="fa-solid fa-circle fa-2xs"></i>
+                <div className="reviewAvg">{spot.numReviews} reviews</div>
+            </>
+        )
+    } else {
+        reviewLine = "";
+    }
+
     return(
         <>
             <i className="fa-sharp fa-solid fa-star"/>
             <div className="reviewAvg">{avgRating}</div>
-            <i class="fa-solid fa-circle fa-2xs"></i>
-            <div className="reviewAvg">{spot.numReviews} reviews</div>
+            {reviewLine}
         </>
     )
 };
--- frontend/src/components/Reviews/index.js
+++ frontend/src/components/Reviews/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import * as reviewActions from "../../store/reviews.js";
 import ReviewList from "./ReviewList.js";
@@ -7,20 +7,34 @@ import "./Review.css";
 
 const Reviews = ({spot}) => {
     const dispatch = useDispatch();
-    const allReviews = useSelector(state => Object.values(state.reviews));
-    console.log(spot)
+    const [isLoaded, setIsLoaded] = useState(false);
+    const spotId = spot.id;
+    const user = useSelector(state => state.session.user);
+    const allReviews = useSelector(state => Object.values(state.reviews).filter((review)=>{
+        if(review.spotId === spotId) return true;
+        return false;
+    }));
     useEffect(()=> {
-        dispatch(reviewActions.loadAllReviews(spot.id));
-    },[dispatch, spot.id]);
+        dispatch(reviewActions.loadAllReviews(spotId))
+        .then(()=>setIsLoaded(true))
+    },[dispatch, spotId]);
+
+    const sorted = allReviews.sort((a,b) => new Date(b.createdAt) - new Date(a.createdAt));
 
     return(
         <>
-            <div>
-                <ReviewAvg spot={spot}/>
-            </div>
-            <div>
-                {allReviews.map(el => (<ReviewList key={el.id} reviewObj={el}/>))}
-            </div>
+            {isLoaded && (
+                <>
+                    <div>
+                        <ReviewAvg spot={spot}/>
+                    </div>
+                    {(user && allReviews.length === 0 && user.id !== spot.Owner.id) ? (
+                        <div>Be the first to review!</div>
+                    ) : (
+                        <>{sorted.map(el => <ReviewList key={el.id} reviewObj={el}/>)}</>
+                    )}
+                </>
+            )}
         </>
     )
 };
