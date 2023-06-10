cc8f1a6763d62940685a5014d8b14a234eb41e03
Nicol Yoshikawa <nicolyoshikawa@Nicols-MBP.attlocal.net>
2023-06-10 14:56:43 -0700

    media screen fix

--- frontend/src/components/Spots/SpotImages.js
+++ frontend/src/components/Spots/SpotImages.js
@@ -2,12 +2,12 @@ import "./Spots.css";
 
 const SpotImages = ({spot}) => {
     let count = 1;
-
+    console.log(spot.SpotImages)
     return (
         <div className="spotImageDetailPage">
             {spot.SpotImages.map(el => (
                 // <div className={el.preview ? "previewImg" : `img${count++}`}>
-                    <img key={el.id} src={`${el.url}`} alt={spot.description} className={el.preview ? "previewImg" : `img${count++}`}/>
+                    <img key={el.id} src={el.url ? `${el.url}` : "https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg"} alt={spot.description} className={el.preview ? "previewImg" : `img${count++}`}/>
                 // </div>
             ))}
         </div>
