dc51be9af5e151b095cac875a273edb6a57826d0
Nicol Yoshikawa <nicolyoshikawa@Nicols-MBP.attlocal.net>
2023-06-09 22:14:25 -0700

    spot detail image format

--- frontend/src/components/Spots/SpotImages.js
+++ frontend/src/components/Spots/SpotImages.js
@@ -1,12 +1,12 @@
 import "./Spots.css";
 
 const SpotImages = ({spot}) => {
+    let count = 1;
+
     return (
         <div className="spotImageDetailPage">
             {spot.SpotImages.map(el => (
-                <div className="spotImageDetailPage">
-                    <img key={el.id} src={`${el.url}`} alt={spot.description}/>
-                </div>
+                <img key={el.id} src={`${el.url}`} alt={spot.description} className={el.preview ? "previewImg" : `img${count++}`}/>
             ))}
         </div>
     )
