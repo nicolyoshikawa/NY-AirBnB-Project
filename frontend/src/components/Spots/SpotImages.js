a3312f42f1f59a691d2fdfb9ff6e83bb7c678ef2
Nicol Yoshikawa <nicolyoshikawa@Nicols-MBP.attlocal.net>
2023-06-10 18:11:12 -0700

    spot images default

--- frontend/src/components/Spots/SpotImages.js
+++ frontend/src/components/Spots/SpotImages.js
@@ -2,14 +2,28 @@ import "./Spots.css";
 
 const SpotImages = ({spot}) => {
     let count = 1;
-    // console.log(spot.SpotImages)
+    let imgArr = [];
+    const images = () => {
+        // console.log("spotImages", spot.SpotImages)
+        for(let i = 0; i < 5; i++){
+            if(spot.SpotImages[i]){
+                let el = spot.SpotImages[i];
+                imgArr.push(<img key={el.id} src={`${el.url}`} alt={spot.description} className={el.preview ? "previewImg" : `img${count++}`}/>)
+            } else {
+                imgArr.push(<img key={`noImage_${i}`}
+                    src={"https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg"}
+                    alt={"noImage"}
+                    className={`img${count++}`}
+                />)
+            }
+        }
+    }
+
+    images();
+    // console.log(imgArr)
     return (
         <div className="spotImageDetailPage">
-            {spot.SpotImages.map(el => (
-                // <div className={el.preview ? "previewImg" : `img${count++}`}>
-                    <img key={el.id} src={el.url ? `${el.url}` : "https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg"} alt={spot.description} className={el.preview ? "previewImg" : `img${count++}`}/>
-                // </div>
-            ))}
+            {imgArr.map((el)=> el)}
         </div>
     )
 };
