0c6467bc345a3507b3a7c65ef33ad7f3c6230ba0
Nicol Yoshikawa <nicolyoshikawa@Nicols-MBP.attlocal.net>
2023-10-21 23:26:41 -0700

    additional spots and spot images

--- backend/db/seeders/20230420230808-spotImages.js
+++ backend/db/seeders/20230420230808-spotImages.js
@@ -13,18 +13,78 @@ module.exports = {
     options.tableName = 'SpotImages';
     return queryInterface.bulkInsert(options, [
       {
-        url: "https://user-images.githubusercontent.com/112415366/233506013-ed9f81aa-82ba-4285-a41c-8dd3b5e720fe.png",
+        url: "",
         spotId: 1,
         preview: true
       },
       {
-        url: "https://user-images.githubusercontent.com/112415366/233506138-6779fbf9-3557-402a-9510-757cc4ec10dc.jpeg",
+        url: "",
         spotId: 2,
         preview: true
       },
       {
-        url: "https://user-images.githubusercontent.com/112415366/233506288-b08a9fa2-7148-402c-b30a-83ae65fab4a6.jpeg",
+        url: "",
         spotId: 3,
+        preview: true
+      },
+      {
+        url: "",
+        spotId: 4,
+        preview: true
+      },
+      {
+        url: "",
+        spotId: 5,
+        preview: true
+      },
+      {
+        url: "",
+        spotId: 6,
+        preview: true
+      },
+      {
+        url: "",
+        spotId: 7,
+        preview: true
+      },
+      {
+        url: "",
+        spotId: 8,
+        preview: true
+      },
+      {
+        url: "",
+        spotId: 9,
+        preview: true
+      },
+      {
+        url: "",
+        spotId: 10,
+        preview: true
+      },
+      {
+        url: "",
+        spotId: 11,
+        preview: true
+      },
+      {
+        url: "",
+        spotId: 12,
+        preview: true
+      },
+      {
+        url: "",
+        spotId: 13,
+        preview: true
+      },
+      {
+        url: "",
+        spotId: 14,
+        preview: true
+      },
+      {
+        url: "",
+        spotId: 15,
         preview: true
       },
     ], {});
